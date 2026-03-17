60c734afbb369ac95c04f0eaf760d981a0232664
techexplorer152 <sonycena844@gmail.com>
2026-03-17 23:12:40 +0100

    routing fix

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,29 +1,27 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Hero from './pages/Home/Hero/Hero';
 import Finance from './pages/Finance Page/Finance';
 import Footer from './components/Footer/footer';
 
 function App() {
     return (
-        <Router>
-            <div className="app-container">
-                <Routes>
+        <div className="app-container">
+            <Routes>
 
-                    <Route path="/" element={<Hero />} />
+                <Route path="/" element={<Hero />} />
 
 
-                    <Route
-                        path="/finance"
-                        element={
-                            <>
-                                <Finance />
-                                <Footer />
-                            </>
-                        }
-                    />
-                </Routes>
-            </div>
-        </Router>
+                <Route
+                    path="/finance"
+                    element={
+                        <>
+                            <Finance />
+                            <Footer />
+                        </>
+                    }
+                />
+            </Routes>
+        </div>
     );
 }
 
