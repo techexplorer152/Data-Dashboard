07271b07cd28a910df4d0d4392dfa3a3091abc3b
techexplorer152 <sonycena844@gmail.com>
2026-03-20 20:36:37 +0100

    added the nav bar and fixed an error

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,14 +2,15 @@ import { Routes, Route } from 'react-router-dom';
 import Hero from './pages/Home/Hero/Hero';
 import Finance from './pages/Finance Page/Finance';
 import Footer from './components/Footer/footer';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
     return (
         <div className="app-container">
+            <Navbar />
+
             <Routes>
-
                 <Route path="/" element={<Hero />} />
-
 
                 <Route
                     path="/finance"
--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -5,10 +5,10 @@ import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-      <BrowserRouter>
-          <App />
-      </BrowserRouter>
+    <StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
 
-  </StrictMode>,
+    </StrictMode>,
 )
