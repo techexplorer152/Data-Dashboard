f45f741d3d083b0c5af4de928e25a15d724305b9
techexplorer152 <sonycena844@gmail.com>
2026-03-27 22:29:08 +0100

    Working on the contact page

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,9 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
+// @ts-ignore
 import Hero from './pages/Home/Hero/Hero';
 import Finance from './pages/Finance Page/Finance';
 import Footer from './components/Footer/footer';
 import Navbar from './components/Navbar/Navbar';
 import Home from './pages/Home/Home'
+import About from './pages/About/About'
+import Contacts from './pages/Contact/Contacts'
 
 function App() {
     return (
@@ -12,6 +15,8 @@ function App() {
 
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About/>}/>
+                <Route path="/contact" element={<Contacts/>}/>
 
                 <Route
                     path="/finance"
---
+++ Frontend/src/pages/Contact/Contacts.tsx
@@ -0,0 +1,24 @@
+import styles from './Contacts.module.css'
+
+ function Contacts( ) {
+    return(
+        <div className={styles.container}>
+           <div className={styles.left_part}>
+               <h2 className={styles.big_title}>
+                   Got an idea? Reach out to talk!
+               </h2>
+
+           </div>
+           <div className={styles.right_part}>
+               <h2>test</h2>
+
+           </div>
+
+
+
+        </div>
+    )
+
+ }
+
+ export default Contacts;
