69b167ccfc34b8e377bbc70ac9af97c42e022407
techexplorer152 <sonycena844@gmail.com>
2026-06-01 23:55:10 +0200

    Update

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,7 +1,4 @@
-
 import { Routes, Route } from 'react-router-dom';
-
-
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/footer';
 import Home from './pages/Home/Home';
@@ -10,28 +7,21 @@ import Contacts from './pages/Contact/Contacts';
 import Services from './pages/Services/Services.js';
 import Finance from './pages/Finance Page/Finance';
 import Text from './pages/Home/Introduction/text';
-
-
 import HeroBackground from './pages/Home/Hero/HeroBackground/HeroBackground';
 import FloatingGdpCard from './pages/Home/Hero/HeroBackground/FloatingCards/Templates/FloatingGdpCard';
 import SportsCard from './pages/Home/Hero/HeroBackground/FloatingCards/Templates/SportsCard';
 import HealthCard from './pages/Home/Hero/HeroBackground/FloatingCards/Templates/HealthCard';
-
 import './App.css';
 
 function App() {
     return (
         <div className="app-container">
             <Navbar />
-
-
-            <div style={{display: 'flex',flexDirection: 'column', alignItems: 'center', justifyContent: 'center',gap:"20px"}}>
-
+            <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', gap: "20px"}}>
                 <div id="card-2"><SportsCard /></div>
                 <div id="card-3"><HealthCard /></div>
                 <div id="card-1"><FloatingGdpCard /></div>
             </div>
-
             <main className="content-wrapper" style={{ position: 'relative', zIndex: 1 }}>
                 <Routes>
                     <Route path="/" element={<Home />} />
@@ -50,10 +40,7 @@ function App() {
                 </Routes>
                 <Text />
             </main>
-
-
             <HeroBackground />
-
             <Footer />
         </div>
     );
